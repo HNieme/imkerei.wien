f65b61387a7d17dd5c9429c313e1cd8ebfa55bd5
Harald Niemeczek <postmaster@niemeczek.at>
2018-04-10 23:01:45 +0200

    neuer Blogeintrag vom 07.04.2018

--- blog/2018/index.php
+++ blog/2018/index.php
@@ -18,12 +18,34 @@
 						<img src="img/2018_03_10_bienen-zwischen-rähmchen-small.jpg" alt="Blick von oben zwischen die Rähmchen"  class="img-fullwidth">
 						<br /><br />						
 						Auch von oben können die Bienen durch die Kunststofffolie, mit der die Rähmchen abgedeckt sind, beobachtet werden.
-						<br /><br />
-						<br />
+						<br /><br /><br />
 					</p>
 					<h3 id="17.03"><a href="#17.03" >17. März</a></h3>
 					<p>
-						Jetzt ist es doch wieder kalt geworden :/ Heute bekommen meine Bienen nur etwas Futterteig (feiner Zucker mit wenig Wasser zu einer festen Masse vermischt), danach lasse ich sie wieder in Ruhe.
+						Jetzt ist es doch wieder kalt geworden :/ 
+						<br />
+						Trotzdem wird schon ein wenig gebrütet. Das erkenne ich an dem Kondenswasser an der Folie, mit der die Bienen abgedeckt sind. Damit steigt dann auch der Verbrauch an Zucker und Pollen drastisch an. Damit meine Bienen jetzt im Frühling nicht noch verhungern bekommen sie etwas Futterteig (feiner Zucker mit wenig Wasser zu einer festen Masse vermischt), danach lasse ich sie wieder in Ruhe.
+						<br /><br /><br />
+					</p>
+					<h3 id="07.04"><a href="#07.04" >Samstag, 7. April</a></h3>
+					<p>
+						Seit fast einem Jahr stehen meine Beuten (so heißen die Kästen, in denen Bienen wohnen) Wind und Wetter (und vor allem Regen) ausgesetzt am Zentralfriedhof herum.
+						Mittlerweile haben die Elemente schon ihre Spuren hinterlassen und es ist mehr als Zeit das Holz vor weiterer Verwitterung zu schützen.
+						Daher kommen heute die gestern gekauften Pinsel und Farbroller zum Einsatz.
+						
+						<br /><br />
+						Den Bienen ist es relativ egal welche Farbe ihre Behausung hat, wild-lebende Bienenvölker wohnen ja auch meist in hohlen Bäumen. Sie orientieren sich hauptsächlich am Ort des Flugloches, also des Eingangs in ihre Behausung. Wird der aber auch nur um einen halben Meter verschoben kennen sich die Bienen nicht mehr aus. Dann fliegen sie verwirrt am Ort des alten Einganges herum. Erst nach einiger Zeit finden sie den Eingang wieder.
+						<br /><br />
+						Da ich Lebensmittel produzieren möchte dürfen die Farben keine Giftstoffe enthalten, da diese unter Umständen in den Honig gelangen könnten. Daher habe ich mir für Kinderspielzeug geeignete Farben gekauft.
+						<br /><br />
+						<img src="img/2018_04_06_beuten_streichen.jpg" alt="Ich beim Streichen meiner Beuten"  class="img-fullwidth">
+						<br /><br />
+						Mit tatkräftiger Unterstützung (hier gerade hinter der Kamera) werden alle Teile mit Schleifpapier abgeschliffen, lackiert, wieder angeschliffen und ein zweites mal lackiert.
+						<br /><br />
+						<img src="img/2018_04_06_beuten_teilweise_gestrichen.jpg" alt="Beuten teilweise gestrichen"  class="img-fullwidth">
+						Nach beinahe sechs Stunden Arbeit sind zwei der drei Beuten fertig gestrichen. Fast zwei Drittel der für neun Quadratmeter geeigneten Farbe haben wir verbraucht! 
+						<br />
+						Genug für heute, nächsten Samstag geht's weiter!
 						<br /><br />
 					</p>
 		<?php 
