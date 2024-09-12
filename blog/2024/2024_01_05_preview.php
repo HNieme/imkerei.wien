d3878f9e43a7871c5edae4bcf99e81706d55e4ca
Harald Niemeczek <git@niemeczek.at>
2024-09-12 15:23:46 +0200

    Add 2024 blog entries

---
+++ blog/2024/2024_01_05_preview.php
@@ -0,0 +1,7 @@
+				<a class="preview" href="/blog/2024/#05.01">
+					<img src="/blog/2024/img/2024_04_07_preview.jpg" alt="Ich beim Streichen">
+					<div>
+						<span class="preview-title">Fr, 05. Jänner 2024 - Muss nur noch kurz die Mails ... äh ... Bienen checken ... !</span>
+						<span class="preview-text">Ich glaube jeder Imker und jede Imkerin kennt das: "Ich fahr nur schnell zu den Bienen!" </br>Tja, schnell geht bei den Bienen nichts ... </span>
+					</div>
+				</a>
---
+++ blog/2024/index.php
@@ -0,0 +1,21 @@
+		<?php 
+			include "../../assets/template/head/head.php";
+		?>
+					<h1>2018</h1>
+					<img src="img/header-beuten-small.jpg" alt="Drei Bienenstöcke"  class="img-fullwidth">
+					<p><br /><br /></p>
+					<h3 id="05.01"><a href="#05.01" >5. Jänner</a></h3>
+					<p>
+                        Ich glaube jeder Imker und jede Imkerin kennt das: "Ich fahr nur schnell zu den Bienen!"
+                        <br /><br />
+                        Tja, schnell geht bei den Bienen nichts ... Außer vielleicht, dass man die Zeit schnell vergessen hat :)
+                        <br /><br />
+                        Jedenfalls geht die Sonne schon unter, es war doch ein bisschen mehr als erwartet zu erledigen und ein herrlich sonniger Tag geht zu Ende.
+
+						<img src="img/2024_01_05_Bienenvölker-im-Winter.png" alt="Bunt angestrichene Bienenstöcke, beleuchtet von goldener Abendsonne"  class="img-fullwidth">
+					</p>
+
+
+		<?php 
+			include "../../assets/template/footer/footer.php";
+		?>
