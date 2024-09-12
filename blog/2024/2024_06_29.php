48594966c5b62e9f39a3baeb37078be9e4d15c51
Harald Niemeczek <git@niemeczek.at>
2024-09-12 20:51:11 +0200

    Add 2024 blog entries

---
+++ blog/2024/2024_06_29.php
@@ -0,0 +1,15 @@
+					<p><br /><br /></p>
+					<h3 id="29.06"><a href="#29.06" >29. Juni - </a></h3>
+					<p>
+					    <br /><br />
+					    Wenn man als Imker einen Anruf bekommt, dass da ein Schwarm im Baum hängt, dann heißt es schnell handeln.<br />
+					    Erstens könnten die Bienen bereits einen "guten" Wohnort in einem Schornstein oder Dachboden gefunden haben und alsbald dort einziehen (und würde dann entweder vom Kammerjäger eliminiert werden oder den Winter aufgrund der Varroamilben nicht überleben) und zweitens sind Schwärme auch bei Imkern ziemlich beliebt.
+					    <br /><br />
+					    Glücklicherweise waren die Damen nur einige Minuten mit dem Rad von meinem Bienenstand entfernt. So wurde kurzerhand eine der beiden Beuten zur Schwarmfangbox umfunktioniert und auf den Gepäckträger am Fahrrad montiert.
+					    <br /><br />
+					    Beim Schwarm angekommen stellte sich heraus: Die hängen ziemlich hoch oben! So wurde die Schwarmfangaktion zu einer schweißtreibenden Kletteraktion auf einen morschen Kirschbaum in einem dichten Gebüsch. Bei der Hitze, voll eingekleidet im Imkeranzug eine schweißtreibende Aktion, die schlussendlich aber erfolgreich war :)
+					    <br /><br />
+					    Am Foto zu sehen: Der eingefangene Schwarm in der Beute - Einige Bienen sterzeln am Eingang und weisen so ihren Schwestern den Weg in das neue Zuhause
+					    <br /><br />
+					    <img src="img/2024_06_29_Schwarmfangequipment.jpg" alt="Ein Schwarm ist in eine Beute eingezogen"  class="img-fullwidth">
+					</p>
--- blog/2024/index.php
+++ blog/2024/index.php
@@ -4,7 +4,7 @@
 					<h1>2024</h1>
 					<img src="img/header-beuten-small.jpg" alt="Drei Bienenstöcke"  class="img-fullwidth">
 					<p><br /><br /></p>
-					<h3 id="05.01"><a href="#05.01" >5. Jänner</a></h3>
+					<h3 id="05.01"><a href="#05.01" >5. Jänner - Erster Besuch bei den Bienen</a></h3>
 					<p>
                         Ich glaube jeder Imker und jede Imkerin kennt das: "Ich fahr nur schnell zu den Bienen!"
                         <br /><br />
@@ -15,7 +15,7 @@
 						<img src="img/2024_01_05_Bienenvölker-im-Winter.png" alt="Bunt angestrichene Bienenstöcke, beleuchtet von goldener Abendsonne"  class="img-fullwidth">
 					</p>
 					<p><br /><br /></p>
-					<h3 id="10.01"><a href="#10.01" >10. Jänner</a></h3>
+					<h3 id="10.01"><a href="#10.01" >10. Jänner - AFB-Probenentnahme oder ein "Gurgel-Test" für Bienen</a></h3>
 					<p>
 					    Im Winter lassen die Bienen immer ein paar "Bröseln" fallen. Hauptsächlich sind das aufgenagte Wabendeckeln um an den Honig zu kommen, aber auch Varroa-Milben, die bei der Behandlung im Winter abgefallen sind und anderer Abfall.
 					    <br /><br />
@@ -27,7 +27,7 @@
 					    <img src="img/2024_01_29_AFB-Probe.jpg" alt="AFB-Probe, Wachsbröseln in Plastikdöschen"  class="img-fullwidth">
 					</p>
 					<p><br /><br /></p>
-					<h3 id="16.02"><a href="#16.02" >16. Februar</a></h3>
+					<h3 id="16.02"><a href="#16.02" >16. Februar - Ein geruhsamer Imker-Monat</a></h3>
 					<p>
 					    <br /><br />
 					    Für mich ist der Februar immer einer der entspanntesten Monate im Imker-Jahr. Es ist noch zu kalt um nachzusehen wie es den Bienen geht (Ende März/Anfang April steht das meist am Plan), aber die Vorfreude ist definitiv schon da.
@@ -36,11 +36,11 @@
 					    <br /><br />
 					    Da kann ich nur sagen: Lasst es euch schmecken!
 					    <br /><br />
-					    <img src="img/2024_02_16_Biene-auf-Mandelblüte-Detailaufnahme.jpg" alt="Biene auf schneeweißer Mandelblüte, Detailaufnahme"  class="img-fullwidth">
-					    <img src="img/2024_02_16_Biene-auf-Mandelblüte.jpg" alt="Biene auf Mandelblüte"  class="img-fullwidth">
+					    <img src="img/2024_02_16_Biene-auf-Schneerose-Detailaufnahme.jpg" alt="Biene auf schneeweißer Mandelblüte, Detailaufnahme"  class="img-fullwidth">
+					    <img src="img/2024_02_16_Biene-auf-Schneerose.jpg" alt="Biene auf Mandelblüte"  class="img-fullwidth">
 					</p>
 					<p><br /><br /></p>
-					<h3 id="28.02"><a href="#28.02" >28. Februar</a></h3>
+					<h3 id="28.02"><a href="#28.02" >28. Februar - Propolistropfen</a></h3>
 					<p>
 					    <br /><br />
 					    Die Propolis-Ernte des letzten Jahres will zu Propolis-Tropfen verarbeitet werden:
@@ -49,7 +49,7 @@
 					    <br /><br />
 					    Kräftig geschüttelt wird von nun an täglich während der nächsten Wochen, so lösen sich all jene Inhaltsstoffe aus dem Rohpropolis, die später hochqualitative Propolistropfen ausmachen.
 					    <br /><br />
-					    <img src="img/2024_02_28_Glas-mit-Propolistropen.jpg" alt="Einmachglas mit Propolis und reinem Alkohol"  class="img-fullwidth">
+					    <img src="img/2024_02_28_Glas-mit-Propolistropfen.jpg" alt="Einmachglas mit Propolis und reinem Alkohol"  class="img-fullwidth">
 					    <img src="img/2024_02_28_Propolis.jpg" alt="Propolis auf weißem Papier"  class="img-fullwidth">
 					</p>
 					<p><br /><br /></p>
@@ -74,7 +74,19 @@
 					    <img src="img/2024_02_29_Biene-auf-Winter-Heckenkirsche-4.jpg" alt="Biene auf Blüte von Winter-Heckenkirsche"  class="img-fullwidth">
 					</p>
 					<p><br /><br /></p>
-					<h3 id="30.03"><a href="#30.30" >30. März</a></h3>
+					<h3 id="29.03"><a href="#29.03" >29. März - Mehr Wohnraum bitte!</a></h3>
+					<p>
+					    <br /><br />
+					    Weil die Familie aktuell rapide wächst (aktuell bis zu 1000 neue Bienen - täglich!) braucht es auch mehr Platz!
+					    <br /><br />
+					    Die Natur ist dieses Jahr ziemlich früh dran, daher sind schon Anfang April in allen Völkern alle zehn Rähmchen voll mit Bienen besetzt. Die Königin will aktuell aber bis zu 2000 Eier legen (auf eine Wabenfläche von etwa zweieinhalb Quadratdezimetern), die in ein paar Wochen schlüpfen sollen. Daher bekamen alle Völker eine zweite Zarge mit zehn weiteren Rähmchen. Und in ein Volk musste die neugierige Imkernase hineingesteckt werden - viel mehr Störung gab es nicht, die kräftigen Völker sind ein eindeutiges Zeichen dafür, dass es den Königinnen gut geht.
+					    <br /><br />
+					    <img src="img/2024_03_29_Bienenwabe.jpg" alt="Bienenwabe"  class="img-fullwidth">
+					    <img src="img/2024_03_29_Bienenwabe-detail.jpg" alt="Bienenwabe Detailansicht"  class="img-fullwidth">
+					    <img src="img/2024_03_29_Offenes-Bienenvolk.jpg" alt="offenes Bienenvolk"  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="30.03"><a href="#30.03" >30. März - Frisch aus dem Briefkasten</a></h3>
 					<p>
 					    <br /><br />
 					    Frisch aus dem Briefkasten:<br />
@@ -84,6 +96,64 @@
 					    <br /><br />
 					    <img src="img/2024_03_30_Laborbefund.jpg" alt="Laborbefund"  class="img-fullwidth">
 					</p>
+					<p><br /><br /></p>
+					<h3 id="16.05"><a href="#16.05" >16. Mai - Neue Königinnen im Anflug!</a></h3>
+					<p>
+					    <br /><br />
+					    Für ein paar eigene Ableger sind gerade Königinnen am Heranwachsen. Dafür habe ich einen Ableger erstellt, der keine Königin hatte. Dieser bekam ein ein Rähmchen mit künstlichen Weiselzellen, in denen ich ganz junge Bienen-Larven platziert hatte. Nach ein paar Tagen waren die Weiselzellen verdeckelt. Die weißen Käfige (Wir Imker nennen sie Lockenwickler) verhindern, dass die erste Königin, die schlüpft alle anderen Königinnen absticht.
+					    <br /><br />
+					    <img src="img/2024_05_16_Weiselrähmchen.jpg" alt="Weiselrähmchen"  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="24.05"><a href="#24.05" >24. Mai - Imposantes Summen</a></h3>
+					<p>
+					    <br /><br />
+					    Nachdem die Königinnen geschlüpft sind, kam jede von Ihnen mit etwa 200ml Bienen aus dem Ableger in ein Begattungskästchen. Die Menge von Bienen in Millilitern anzugeben klingt seltsam, aber tatsächlich lassen sich die Bienen, wenn sie nass gesprüht wurden mit einem Messbecher in Millilitern abmessen.
+					    <br /><br />
+					    Dann kamen alle Begattungskästchen für drei Tage in den Keller. Ich bin jedes Mal erstaunt wie laut Bienen summen können!
+					    <br /><br />
+					    <img src="img/2024_05_24_Begattungskästchen-im-Keller.png" alt="Begattungskästchen im Keller"  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="27.05"><a href="#27.05" >27. Mai - Achtung, Abflug!</a></h3>
+					<p>
+					    <br /><br />
+					    Nach drei Tagen im Keller dürfen die Bienen endlich wieder fliegen - An einem anderen Standplatz aufgestellt werden die Damen die nächsten zwei Wochen verbringen. Danach legen hoffentlich alle Königinnen Eier und dürfen in einen größeren Ableger umziehen :)
+					    <br /><br />
+					    Sobald das Flugloch geöffnet wird herrscht große Aufregung! Viele Bienen wollen hinaus und fliegen sich auf ihr neues Zuhause ein. In zuerst kleinen, dann immer größer werdenden Bögen um das Flugloch, stets mit dem Kopf zum Begattungskästchen gewandt, wird die Umgebung erkundet. Schlussendlich geht es wieder zurück zu den anderen.
+					    <br /><br />
+					    Um den Weg zu weisen sitzen rund um den Eingang einige "sterzelnde" Bienen - Bienen haben eine Pheromondrüse im hintersten Ring des Unterleibs. Diese wird beim sterzeln freigelegt und die Pheromone durch Fächern mit den Flügeln in der Luft verteilt.
+					    <br /><br />
+					    <img src="img/2024_05_27_Begattungskästchen.jpg" alt=""  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="30.03"><a href="#30.03" >30. März - Es Lindet!</a></h3>
+					<p>
+					    <br /><br />
+					    Ziemlich früh blühen heuer die Linden in Wien und füllen die Honigzargen mit jedem Tag etwas mehr. Immerhin ist die Linde in Wien wohl die absolute Trachtpflanze Nummer eins, die auch für den kräftig aromatischen Stadthonig sorgt.
+					    <br /><br />
+					    Apropos Honig - Die Vorbereitungen fürs Schleudern laufen schon, Ende Juni/Anfang August sollte es soweit sein :)
+					    <br /><br />
+					    <img src="img/2024_06_07_Lindenblüten.jpg" alt="Eine stark blühende Linde"  class="img-fullwidth">
+					    <img src="img/2024_06_07_Lindenblüten-mit-Biene.jpg" alt=""  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="26.06"><a href="#26.06" >26. Juli - Bienen von Rubicon IT</a></h3>
+					<p>
+					    <br /><br />
+					    Die Firma Rubicon IT kümmert sich um den Bienenbestand und stellt mir zwecks Vermehrung desselben zwei Bienenvölker zur Verfügung! Danke dafür!
+					    <br /><br />
+					    Zwei Beuten (Imkerlatein für die Holz- oder Styroporboxen, in denen die Bienen gehalten werden) stehen bereit - jetzt fehlen nur noch die Bienen. Große Vorfreude kommt auf :)
+					    <br /><br />
+                        Die Beuten sind übrigens selbst gebaut, als modifizierte Version der Hohenheimer Einfachbeute (Eine Anleitung findet sich hier auf der Webseite). Und wo immer es geht, werden Dinge recycelt oder wiederverwendet - so sind zum Beispiel die Blechabdeckungen der Deckeln Reste unseres alten Blechdaches vom Haus und die roten Balken, auf denen die Bienenbeuten stehen durfte ich mir aus einem Baucontainer mitnehmen. Wer jetzt aber glaubt, dass die Imkerei ein billiges Hobby wäre ... weit gefehlt! Alleine die rote Lasur für die Balken unter diesen und einigen zusätzlichen Beuten hat fast 30€ gekostet und eine ganze Dose verschlungen!
+					    <br /><br />
+					    <img src="img/2024_06_26_Bienenbeuten.jpg" alt="Zwei Bienenbeuten mit Honigglas und Smoker am Dach"  class="img-fullwidth">
+					</p>
+					<?php
+                        include "2024_06_29.php";
+                    ?>
+
+
 
 
 		<?php 
