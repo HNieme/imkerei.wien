d13780a6c2ec2d4bd648df5157b0384b31d54fe0
Harald Niemeczek <git@niemeczek.at>
2024-09-12 15:43:08 +0200

    Add 2024 blog entries

--- blog/2024/index.php
+++ blog/2024/index.php
@@ -1,7 +1,7 @@
 		<?php 
 			include "../../assets/template/head/head.php";
 		?>
-					<h1>2018</h1>
+					<h1>2024</h1>
 					<img src="img/header-beuten-small.jpg" alt="Drei Bienenstöcke"  class="img-fullwidth">
 					<p><br /><br /></p>
 					<h3 id="05.01"><a href="#05.01" >5. Jänner</a></h3>
@@ -11,10 +11,21 @@
                         Tja, schnell geht bei den Bienen nichts ... Außer vielleicht, dass man die Zeit schnell vergessen hat :)
                         <br /><br />
                         Jedenfalls geht die Sonne schon unter, es war doch ein bisschen mehr als erwartet zu erledigen und ein herrlich sonniger Tag geht zu Ende.
-
+                        <br /><br />
 						<img src="img/2024_01_05_Bienenvölker-im-Winter.png" alt="Bunt angestrichene Bienenstöcke, beleuchtet von goldener Abendsonne"  class="img-fullwidth">
 					</p>
-
+					<p><br /><br /></p>
+					<h3 id="10.01"><a href="#10.01" >10. Jänner</a></h3>
+					<p>
+					    Im Winter lassen die Bienen immer ein paar "Bröseln" fallen. Hauptsächlich sind das aufgenagte Wabendeckeln um an den Honig zu kommen, aber auch Varroa-Milben, die bei der Behandlung im Winter abgefallen sind und anderer Abfall.
+					    <br /><br />
+					    Diese Bröseln landen dann auf einem Kunststoff-Schieber. Ich sammle diese jedes Jahr ein, fülle sie in einen Probenbehälter und gebe diesen bei meinem Imkerverein ab, der sie gesammelt mit anderen Proben in ein Labor versendet. Dort wird untersucht, ob meine Bienen Amerikanische Faulbrut (eine hochansteckende Seuche) haben. Falls nicht bekommt man ein Gesundheitszeugnis, das Voraussetzung ist um Bienen beispielsweise auf Belegstellen zu bringen oder mit ihnen zu einer Tracht zu wandern. Auch alle Bienen, die hier auf Hektar Nektar verkauft werden benötigen ein Gesundheitszeugnis.
+					    <br /><br />
+					    Funktioniert also so ähnlich wie ein Gurgeltest, nur auf das Ergebnis muss man länger warten :)
+					    <br /><br />
+					    <img src="img/2024_01_29_Bodenbrett.jpg" alt="Bodenbrett voller Wachsbröseln am Ende des Winters"  class="img-fullwidth">
+					    <img src="img/2024_01_29_AFB-Probe.jpg" alt="AFB-Probe, Wachsbröseln in Plastikdöschen"  class="img-fullwidth">
+					</p>
 
 		<?php 
 			include "../../assets/template/footer/footer.php";
