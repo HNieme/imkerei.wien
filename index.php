7d809fdc3aa8bf24de1a0f85babe711ad716fa76
Harald Niemeczek <git@niemeczek.at>
2024-09-12 23:16:23 +0200

    minor error fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 			include "assets/template/head/head.php";
 		?>
 		<?php
-		    include "blog/2024/article-preview-list.php;
+		    include "blog/2024/article-preview-list.php";
 			include "blog/imkerlatein/beute_preview.php";
 			include "blog/2018/preview.php";
 			include "blog/2017/preview.php";
