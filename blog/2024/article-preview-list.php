a1f48db328bf15c5f05016377837c9d4809cdbbc
Harald Niemeczek <git@niemeczek.at>
2024-09-12 23:20:07 +0200

    minor error fix

--- blog/2024/article-preview-list.php
+++ blog/2024/article-preview-list.php
@@ -1,19 +1,19 @@
 <p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
 <?php
-include "/blog/2024/varroabehandlung_preview.php";
-include "/blog/2024/2024_08_05_preview.php";
-include "/blog/2024/2024_07_02_preview.php";
-include "/blog/2024/2024_06_29_preview.php";
-include "/blog/2024/2024_06_26_preview.php";
-include "/blog/2024/2024_06_07_preview.php";
-include "/blog/2024/2024_05_27_preview.php";
-include "/blog/2024/2024_05_24_preview.php";
-include "/blog/2024/2024_05_16_preview.php";
-include "/blog/2024/2024_03_30_preview.php";
-include "/blog/2024/2024_03_29_preview.php";
-include "/blog/2024/2024_02_29_preview.php";
-include "/blog/2024/2024_02_28_preview.php";
-include "/blog/2024/2024_02_16_preview.php";
-include "/blog/2024/2024_01_29_preview.php";
-include "/blog/2024/2024_01_05_preview.php";
+include "varroabehandlung_preview.php";
+include "2024_08_05_preview.php";
+include "2024_07_02_preview.php";
+include "2024_06_29_preview.php";
+include "2024_06_26_preview.php";
+include "2024_06_07_preview.php";
+include "2024_05_27_preview.php";
+include "2024_05_24_preview.php";
+include "2024_05_16_preview.php";
+include "2024_03_30_preview.php";
+include "2024_03_29_preview.php";
+include "2024_02_29_preview.php";
+include "2024_02_28_preview.php";
+include "2024_02_16_preview.php";
+include "2024_01_29_preview.php";
+include "2024_01_05_preview.php";
 ?>
