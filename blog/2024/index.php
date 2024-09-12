0daf24bed1da0068aec36313a46ce7e7c5540279
Harald Niemeczek <git@niemeczek.at>
2024-09-12 16:09:39 +0200

    Add 2024 blog entries

--- blog/2024/index.php
+++ blog/2024/index.php
@@ -26,6 +26,65 @@
 					    <img src="img/2024_01_29_Bodenbrett.jpg" alt="Bodenbrett voller Wachsbröseln am Ende des Winters"  class="img-fullwidth">
 					    <img src="img/2024_01_29_AFB-Probe.jpg" alt="AFB-Probe, Wachsbröseln in Plastikdöschen"  class="img-fullwidth">
 					</p>
+					<p><br /><br /></p>
+					<h3 id="16.02"><a href="#16.02" >16. Februar</a></h3>
+					<p>
+					    <br /><br />
+					    Für mich ist der Februar immer einer der entspanntesten Monate im Imker-Jahr. Es ist noch zu kalt um nachzusehen wie es den Bienen geht (Ende März/Anfang April steht das meist am Plan), aber die Vorfreude ist definitiv schon da.
+					    <br /><br />
+					    An manch warmem Tag entdecken aufmerksame Augen jedoch schon Bienen, die an ein paar Schneerosen in Blumentöpfen nach Pollen suchen :)
+					    <br /><br />
+					    Da kann ich nur sagen: Lasst es euch schmecken!
+					    <br /><br />
+					    <img src="img/2024_02_16_Biene-auf-Mandelblüte-Detailaufnahme.jpg" alt="Biene auf schneeweißer Mandelblüte, Detailaufnahme"  class="img-fullwidth">
+					    <img src="img/2024_02_16_Biene-auf-Mandelblüte.jpg" alt="Biene auf Mandelblüte"  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="28.02"><a href="#28.02" >28. Februar</a></h3>
+					<p>
+					    <br /><br />
+					    Die Propolis-Ernte des letzten Jahres will zu Propolis-Tropfen verarbeitet werden:
+					    <br /><br />
+					    Zuerst werden alle Fremdkörper per Hand aussortiert - Also Grashalme, Holzspäne usw fische ich mit einer Pinzette aus dem Rohpropolis. Anschließend wird mit reinem Alkohol aufgegossen und kräftig geschüttelt.
+					    <br /><br />
+					    Kräftig geschüttelt wird von nun an täglich während der nächsten Wochen, so lösen sich all jene Inhaltsstoffe aus dem Rohpropolis, die später hochqualitative Propolistropfen ausmachen.
+					    <br /><br />
+					    <img src="img/2024_02_28_Glas-mit-Propolistropen.jpg" alt="Einmachglas mit Propolis und reinem Alkohol"  class="img-fullwidth">
+					    <img src="img/2024_02_28_Propolis.jpg" alt="Propolis auf weißem Papier"  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="29.02"><a href="#29.02" >29. Februar</a></h3>
+					<p>
+					    <br /><br />
+					    Eine Freundin und ich waren kürzlich am Ring-Radweg neben dem Stadtpark unterwegs.
+					    <br /><br />
+					    Für einen Atemzug weht mir der Fahrtwind kräftig süßliche Aromen ins Gesicht. Den Geruch kenne ich! Ich muss die Pflanze nur noch finden!
+					    <br /><br />
+					    Nach einer kurzen Suche stehen wir davor: Ein Strauch mit eher unscheinbaren Blüten aber hervorragendem Geruch. Nicht umsonst heißt die Pflanze, die ich als Winter-Heckenkirsche (Lonicera fragrantissima) kenne auch Wohlriechende Heckenkirsche oder Duft-Heckenkirsche.
+					    <br /><br />
+					    Scheinbar sehen das auch die Bienen so: Der Strauch summt!
+					    <br /><br />
+					    So richtig fotogen waren die Bienchen aber nicht - Allesamt gut eingestaubt mit Pollen und schnell von Blüte zu Blüte unterwegs.
+					    <br /><br />
+					    Für die Honigliebhaber:innen: Ich muss euch enttäuschen - Der hier gesammelte Nektar und Pollen wird wohl nie in einem Glas landen. Der Bienen-Nachwuchs ist hungrig und wird vermutlich alles sofort verspeisen :)
+					    <br /><br />
+					    <img src="img/2024_02_29_Biene-auf-Winter-Heckenkirsche.jpg" alt="Biene auf Blüte von Winter-Heckenkirsche"  class="img-fullwidth">
+					    <img src="img/2024_02_29_Biene-auf-Winter-Heckenkirsche-2.jpg" alt="Biene auf Blüte von Winter-Heckenkirsche"  class="img-fullwidth">
+					    <img src="img/2024_02_29_Biene-auf-Winter-Heckenkirsche-3.jpg" alt="Biene auf Blüte von Winter-Heckenkirsche"  class="img-fullwidth">
+					    <img src="img/2024_02_29_Biene-auf-Winter-Heckenkirsche-4.jpg" alt="Biene auf Blüte von Winter-Heckenkirsche"  class="img-fullwidth">
+					</p>
+					<p><br /><br /></p>
+					<h3 id="30.03"><a href="#30.30" >30. März</a></h3>
+					<p>
+					    <br /><br />
+					    Frisch aus dem Briefkasten:<br />
+					    Der Laborbefund über die AFB-Untersuchung ist da! Wie erwartet sind die Bienen kerngesund und keine Spur der ansteckenden Bienenseuche wurde gefunden!
+					    <br /><br />
+					    Das Bienenjahr kann losgehen! Ich platze schon fast vor Vorfreude, wieder mal unter die Beutendeckel schauen zu können!
+					    <br /><br />
+					    <img src="img/2024_03_30_Laborbefund.jpg" alt="Laborbefund"  class="img-fullwidth">
+					</p>
+
 
 		<?php 
 			include "../../assets/template/footer/footer.php";
