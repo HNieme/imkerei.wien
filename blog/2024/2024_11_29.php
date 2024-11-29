5ab59eb3f93e92b8831beb802b1380019535ff82
Harald Niemeczek <git@niemeczek.at>
2024-11-29 13:13:47 +0100

    Added blog-post Adventkranz

---
+++ blog/2024/2024_11_29.php
@@ -0,0 +1,10 @@
+					<p><br /><br /></p>
+					<h3 id="29.11"><a href="#29.11" >29. November - Adventkranz mit Bienenwachs-Kerzen</a></h3>
+					<p>
+					    <br /><br />
+					    Uns ist ein bisschen Reisig übrig geblieben, mit dem sich ein weiterer Adventkranz ausging.<br />
+                        Kerzen aus 100% Bienenwachs von meinen eigenen Bienen, unter anderem aus dem Stadtwäldchen.<br />
+                        Also wer noch keinen Adventkranz hat, um 38€ ist er zu haben - Vorbestellungen für nächstes Jahr sind auch möglich ;)
+					    <br /><br />
+					    <img src="img/2024_11_28_Adventkranz.jpg" alt=""  class="img-fullwidth">
+					</p>
---
+++ blog/2024/2024_11_29_preview.php
@@ -0,0 +1,9 @@
+				<a class="preview" href="/blog/2024/#2024_11_29">
+					<img src="/blog/2024/img/2024_11_28_Adventkranz_preview.jpg" alt="Adventkranz">
+					<div>
+						<span class="preview-title">29. November - Adventkranz mit Bienenwachs-Kerzen</span>
+						<span class="preview-text">
+						    Uns ist ein bisschen Reisig übrig geblieben, mit dem sich ein weiterer Adventkranz ausging. Kerzen aus 100% Bienenwachs von meinen eigenen Bienen ...
+						</span>
+					</div>
+				</a>
