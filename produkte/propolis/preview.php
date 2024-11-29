13e6e69bb331b76e56c894296e79457b62971dcc
Harald Niemeczek <git@niemeczek.at>
2024-11-29 13:02:34 +0100

    Updated propolis preview

--- produkte/propolis/preview.php
+++ produkte/propolis/preview.php
@@ -2,6 +2,6 @@
 					<img src="/produkte/propolis/img/propolis-preview.jpg" alt="Propolis">
 					<div>
 						<span class="preview-title">Propolis</span>
-						<span class="preview-text">Im Herbst 2017 konnte ich geringe Mengen Propolis ernten.</span>
+						<span class="preview-text">Propolis (auch Kittharz genannt) ist eine harzartige Masse, die von Bienen zum Verschließen von Ritzen und Abdecken von Oberflächen genutzt wird. Durch ...</span>
 					</div>
 				</a>
