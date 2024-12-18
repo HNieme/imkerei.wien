7755296879edb298b7dfe54c788e98f1a3b9b601
Harald Niemeczek <git@niemeczek.at>
2024-12-18 20:04:11 +0100

    Added blog-post Gepanschter Honig

---
+++ blog/2024/2024_12_16.php
@@ -0,0 +1,17 @@
+					<p><br /><br /></p>
+					<h3 id="2024_12_16"><a href="#2024_12_16">16. Dezember - Gepanschter Honig?!</a></h3>
+					<p>
+					    Unter Imkern ist es schon länger bekannt, der breiten Öffentlichkeit jedoch nicht: Wo Honig draufsteht, ist oft kein echter Honig drin.
+					    <br /><br />
+					    Dies zeigt in der Vorweihnachtszeit wieder einmal ein Bericht des ORF (<a href="https://help.orf.at/stories/3228083/">Gepanschter Honig auch in Österreich?</a>)
+					    <br /><br />
+					    Fazit der Recherche: 75% der untersuchten Honige dürften eigentlich gar nicht als Honig verkauft werden.
+					    <br /><br />
+                        Und:<br />
+					    "Was wir [...] daraus lernen, ist, dass wir uns auf österreichischen Honig verlassen können,
+					    nämlich Honig, der von österreichischen Imkern produziert wurde."
+					    (<a href="https://on.orf.at/video/14255583/15781184/honig-test-die-ergebnisse-konkret-vom-16122024">Konkret</a>)
+
+					    Wer den Imker dann noch persönlich kennt und die Bienen bei ihrer Arbeit beobachten kann, der kann sich sicher sein: 100% Honig, 100% echt, 100% köstlich!
+					    <img src="img/2024_12_16_Fake-Honey.png" alt="Fake Honey (KI generiert)"  class="img-fullwidth">
+					</p>
---
+++ blog/2024/2024_12_16_preview.php
@@ -0,0 +1,9 @@
+				<a class="preview" href="/blog/2024/#2024_12_16">
+					<img src="/blog/2024/img/2024_12_16_Fake-Honey_preview.png" alt="Fake Honey (KI generiert)">
+					<div>
+						<span class="preview-title">16. Dezember - Gepanschter Honig?!</span>
+						<span class="preview-text">
+						    Unter Imkern ist es schon länger bekannt, der breiten Öffentlichkeit jedoch nicht: Wo Honig draufsteht, ist oft kein echter Honig drin.
+						</span>
+					</div>
+				</a>
