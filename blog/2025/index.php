c8a6894313b7c0e190ec202d0f0c5bf8cf60c42b
Harald Niemeczek <git@niemeczek.at>
2025-03-17 16:07:43 +0100

    Added folder for 2025 blog articles

---
+++ blog/2025/index.php
@@ -0,0 +1,11 @@
+		<?php 
+			include "../../assets/template/head/head.php";
+		?>
+					<h1>2024</h1>
+					<img src="img/2025_03_08_Bienenwabe_Licht_durchscheinend.jpg" alt="Bienenwabe, fotografiert gegen die Sonne, sodass das Licht durchscheint"  class="img-fullwidth">
+					<?php
+                        include "article-list.php";
+                    ?>
+		<?php 
+			include "../../assets/template/footer/footer.php";
+		?>
---
+++ blog/2025/preview.php
@@ -0,0 +1,7 @@
+				<a class="preview" href="/blog/2018/">
+					<img src="/blog/2024/img/preview.jpg" alt="Drei Bienenvölker">
+					<div>
+						<span class="preview-title">2024 - Ein gutes Bienenjahr?</span>
+						<span class="preview-text">Zusammenfassung des Jahres 2024</span>
+					</div>
+				</a>
