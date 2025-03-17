14ddf892cd2d010dec7842d740de569af3ede39e
Harald Niemeczek <git@niemeczek.at>
2025-03-17 16:36:15 +0100

    Added 2025 blog entry stubs

---
+++ blog/2025/2025_03_08.php
@@ -0,0 +1,8 @@
+					<p><br /><br /></p>
+					<h3 id="2024_06_29"><a href="#2025_03_16" >16. März -  </a></h3>
+					<p>
+					    <br /><br />
+
+					    <br /><br />
+					    <img src="img/2025_03_16_Honig_abgefüllt.jpg" alt=""  class="img-fullwidth">
+					</p>
--- blog/2025/index.php
+++ blog/2025/index.php
@@ -1,7 +1,7 @@
 		<?php 
 			include "../../assets/template/head/head.php";
 		?>
-					<h1>2024</h1>
+					<h1>2025</h1>
 					<img src="img/2025_03_08_Bienenwabe_Licht_durchscheinend.jpg" alt="Bienenwabe, fotografiert gegen die Sonne, sodass das Licht durchscheint"  class="img-fullwidth">
 					<?php
                         include "article-list.php";
