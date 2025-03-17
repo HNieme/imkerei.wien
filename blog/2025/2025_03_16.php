57327ee31e84a12db9089add1eed5e5e674c9daa
Harald Niemeczek <git@niemeczek.at>
2025-03-17 16:36:15 +0100

    Added 2025 blog entry stubs

--- blog/2025/2025_03_16.php
+++ blog/2025/2025_03_16.php
@@ -1,8 +1,10 @@
 					<p><br /><br /></p>
-					<h3 id="2024_06_29"><a href="#2024_06_29" >29. Juni -  </a></h3>
+					<h3 id="2024_06_29"><a href="#2024_06_29" >16. März - Honey, Honey</a></h3>
 					<p>
 					    <br /><br />
-
+                            Der Weg von der Bienenwabe ins Glas ist ein langer und hat mehr Arbeitsschritte, als man auf
+                            den ersten Blick annehmen würde. Was wir Imker für optimale Honigqualität tun und woran auch
+                            du diese erkennst erfährst du in diesem Blogbeitrag.
 					    <br /><br />
-					    <img src="img/" alt=""  class="img-fullwidth">
+					    <img src="img/2025_03_16_Honig_abgefüllt.jpg" alt=""  class="img-fullwidth">
 					</p>
--- blog/2025/2025_03_16_preview.php
+++ blog/2025/2025_03_16_preview.php
@@ -1,7 +1,11 @@
 				<a class="preview" href="/blog/2025/">
-					<img src="/blog/2025/img/preview.jpg" alt="Drei Bienenvölker">
+					<img src="/blog/2025/img/2025_03_16_Honig_abgefüllt_preview.jpg" alt="Zahlreiche Honiggläser">
 					<div>
-						<span class="preview-title">2024 - Ein gutes Bienenjahr?</span>
-						<span class="preview-text">Zusammenfassung des Jahres 2024</span>
+						<span class="preview-title">16. März - Honey, Honey</span>
+						<span class="preview-text">
+						    Der Weg von der Bienenwabe ins Glas ist ein langer und hat mehr Arbeitsschritte, als man auf
+                            den ersten Blick annehmen würde. Was wir Imker für optimale Honigqualität tun und woran auch
+                            du diese erkennst erfährst du in diesem Blogbeitrag.
+						</span>
 					</div>
 				</a>
