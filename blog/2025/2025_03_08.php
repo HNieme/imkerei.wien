67beac580f674a2632d964cf6a2fc94d073cfc37
Harald Niemeczek <git@niemeczek.at>
2025-03-21 15:41:11 +0100

    Added/updated/finished blog articles

--- blog/2025/2025_03_08.php
+++ blog/2025/2025_03_08.php
@@ -2,8 +2,37 @@
 					<h3 id="2024_06_29"><a href="#2025_03_16" >08. März - Auswintern: War es ein guter Winter? </a></h3>
 					<p>
 					    <br /><br />
-                            Der Winter nähert sich dem Ende, jetzt gibt es nach Monaten den ersten Blick ins Bienenvolk.
-                            Sind alle wohlauf? Geht es den Königinnen gut?
+                        Der Winter nähert sich dem Ende, jetzt gibt es nach Monaten den ersten Blick ins Bienenvolk.
+                        Sind alle wohlauf? Geht es den Königinnen gut?
+					    <br /><br />
+					    Zuerst mal zu den harten Zahlen und Fakten:
+					    <br /><br />
+					    Vergangenes Jahr habe ich 7 Bienenvölker ausgewintert,
+					    zwei Völker <a href="../2024/#2024_06_26" >von Rubicon IT gesponsert bekommen</a> (Vielen Dank dafür! - Beide haben überlebt :) )
+					    und einen <a href="../2024/#2024_06_29">Schwarm</a> ganz in der Nähe von einem Baum eingefangen
+					    (Auch hier vielen Dank fürs Bescheid geben!). Zusätzlich habe ich zahlreiche Ableger gebildet,
+					    sodass ich im Oktober mit etwa 30 Völkern in den Winter gegangen bin.
+					    <br /><br />
+					    Von diesen 30 Völkern sind 22 gut über den Winter gekommen. <br />
+					    Bei dem Schwarm-Volk war die Königin wohl schlecht begattet und konnte deshalb nach dem Winter keine
+					    Arbeiterinnen-Eier mehr legen. Daher musste das Volk mit einem anderen, eher schwachen Ableger vereinigt
+					    werden).<br />
+					    Einen Ableger hat ein ähnliches Schicksal ereilt, hier dürfte die Königin ebenfalls verloren
+					    gegangen oder schlecht begattet worden sein.<br />
+					    Die noch fehlenden ~6 Völker haben es nicht über den Winter geschafft - Soweit ich das beurteilen
+					    kann gab es trotz Behandlung im Herbst relativ hohen Varroa-Befall, der für den Verlust der Völker
+					    verantwortlich sein dürfte sowie bei zwei Völkern ein sogenannter Futter-Abriss, also die Bienen
+					    haben das Futter in der Nähe des Platzes, an dem sie saßen aufgebraucht und es dann aufgrund der
+					    Temperaturen nicht mehr zu frischem Futter ein paar Zentimeter weiter geschafft.
+					    <br /><br />
+					    Laut Informationen meines Imkervereins dürfte es allerdings insgesamt sehr hohe Völkerverluste
+					    mit noch unbekannter Ursache in Wien gegeben haben (über 50 Prozent!). In Anbetracht dessen bin
+					    ich mit dem Winter dieses Jahr zufrieden, auch wenn ich mir gewünscht hätte, dass es alle Völker
+					    über den Winter schaffen.
+					    <br /><br />
+					    Fazit für den kommenden Sommer: Früher gegen Varroa behandeln (sofern dies möglich ist - Es darf
+					    im Bienenvolk maximal 35°C haben, bei Hitzewellen ist man da sehr schnell drüber), Ableger
+					    erstellen um eine Reserve für nächsten Winter zu haben und besser die Varroa-Belastung kontrollieren.
 					    <br /><br />
 					    <img src="img/2025_03_08_Bienenwabe_Licht_durchscheinend.jpg" alt="Bienenwabe, fotografiert gegen die Sonne, sodass das Licht durchscheint."  class="img-fullwidth">
 					</p>
--- blog/2025/article-preview-list.php
+++ blog/2025/article-preview-list.php
@@ -1,4 +1,5 @@
 <?php
-include "2025_03_16_preview.php";
+include "2025_03_23_preview.php";
+//include "2025_03_16_preview.php";
 include "2025_03_05_preview.php";
 ?>
