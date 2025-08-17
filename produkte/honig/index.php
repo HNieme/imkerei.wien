5592f8eb327fffda2a02450d5e9771cfb0112246
harald <harald@niemeczek.at>
2025-08-17 14:56:29 +0200

    Updated produkte -> honig

--- produkte/honig/index.php
+++ produkte/honig/index.php
@@ -5,24 +5,45 @@
 			<p><br /><br /></p>
 			<h1>Honig</h1>
 			<p>
-				2024 war ein außergewöhnlich gutes Bienenjahr - der warme Frühling führte zu sehr früh entwickelten Völkern. Die Pflanzen der Umgebung waren gut mit Wasser versorgt und stellten damit eine hervorragende Nektarquelle dar. So kam es zur besten Honigernte bisher!
+				Die Honigernte 2025 ist abgeschlossen und das köstliche flüssige Gold hat den Weg ins Glas gefunden!
 				<br />
 				<br />
 				
 			</p>
 			<h2>Woher genau kommt der Honig? Von welchen Pflanzen ist er?</h2>
 			<p>
-				Meine Bienen standen das ganze Jahr im "Stadtwäldchen" nur wenige Meter entfernt von der U6 Station Am Schöpfwerk.
+				Meine Bienen standen das ganze Jahr im "Stadtwäldchen" nur wenige Meter entfernt von der U6 Station Am Schöpfwerk. Ich schleudere nur ein Mal pro Jahr, meist Ende Juli.
 				<br />
 				<br />
-				In der Umgebung sorgen im Frühjahr <a href="https://de.wikipedia.org/wiki/Kriecherl">Kriacherl</a> und diverse andere Obstbäume für den ersten gesammelten Honig. Später blühen unter anderem in der Oswaldgasse eine ganze Allee Ross-Kastanien sowie im angrenzenden Miep-Gies-Park Linden, Robinien ("Akazien") sowie ein Götterbaum im Stadtwäldchen. Zusätzlich sorgen auch die Gärten und Balkone der Umgebung für willkommene Abwechslung - Vielleicht hat ja auch schon an deinem Balkon eine Biene genascht?
+				In der Umgebung sorgen im Frühjahr <a href="https://de.wikipedia.org/wiki/Kriecherl">Kriacherl</a> und diverse andere Obstbäume für den ersten gesammelten Honig. Später blühen unter anderem in der Oswaldgasse eine ganze Allee Ross-Kastanien sowie im angrenzenden Miep-Gies-Park Robinien ("Akazien") sowie ein Götterbaum im Stadtwäldchen. Ich vermute den Hauptanteil (wie bei den meisten Wiener Honigen) macht aber die Linde mit ihrem kräftigen Aroma. Zusätzlich sorgen auch die Gärten und Balkone der Umgebung für willkommene Abwechslung - Vielleicht hat ja auch schon an deinem Balkon eine Biene genascht?
 				<br />
 				<br />
-				So entsteht ein aromatischer, herrlich schmeckender Honig, der bald als ein Glas süßes Gold den Weg zu dir nach Hause finden könnte :)
+				So entsteht ein intensiver, herrlich schmeckender Honig, der bald als ein Glas süßes Gold den Weg zu dir nach Hause finden könnte :)
 			</p>
+            <h2>Wie lange hält sich Honig</h2>
+            <p>
+                Nun, das kommt darauf an - unter den richtigen Bedingungen kann Honig sehr lange haltbar sein.
+                <br /><br />
+                Was sind die richtigen Bedingungen? Am Etikett muss stehen <i>"Trocken und vor Wärme geschützt lagern"</i> oder ""<i>"Vor Licht und Wärme geschützt lagern"</i>.
+                Und genau das sollte man auch machen. Also Glas gut zuschrauben, niemals offen stehen lassen. Nicht am Fensterbrett oder im heißen Auto liegen lassen.
+                Vorräte am Besten in den (trockenen) Keller stellen.
+                <br /><br />
+                Aus rechtlichen Gründen muss ich am Etikett ein Mindesthaltbarkeitsdatum angeben - ich wähle hier meist ein bis zwei Jahre.
+                <br />
+                Hält man sich an die obigen Tipps wird der Honig voraussichtlich auch lange danach noch gut schmecken.
+                <br />
+                Aber mal ehrlich: Der beste Aufbewahrungsort ist doch ohnehin der Magen ;)
+
+            </p>
+            <h2>Ist der Honig Bio?</h2>
+            <p>
+                Nein - um ein Produkt als "Bio" verkaufen zu dürfen muss man ein aufwändiges und teures Zertifizierungsverfahren durchlaufen und sehr viele Voraussetzungen erfüllen. Für mich ist die Imkerei nur ein Hobby, bei den geringen Mengen an Honig die ich ernte zahlt sich die Zertifizierung nicht aus.
+                <br /><br />
+                Aber ich kann versprechen: In meinem Honig ist nur Honig drin - zu 100% von den Bienenstöcken neben dem Stadtwäldchen, gesammelt von meinen Bienen im Umkreis von bis zu 3 km.
+            </p>
 			<h2>Preise</h2>
 			<p>
-			Der Honig ist in 500g, 250g und 45g Gläsern erhältlich.
+			Der Honig ist in 1000g, 500g, 250g und 45g Gläsern erhältlich.
 			<br /><br />
 			</p>
 			<img src="img/2024_01_26_Honiggl%C3%A4ser.jpg" alt=""  class="img-fullwidth">
@@ -35,9 +56,16 @@
 			            <th>&gt;15 Gläser</th>
 			            <th>1-15 Gläser</th>
 			        </tr>
+                    <tr>
+                		<th>1000g</th>
+                		<td>16,50 €</td>
+                		<td>17,00 €</td>
+                		<td>17,50 €</td>
+                		<td>18,00 € €</td>
+                	</tr>
 			        <tr>
 			            <th>500g</th>
-			            <td>8,00 €</td>
+			            <td>8,25 €</td>
 			            <td>8,50 €</td>
 			            <td>9,00 €</td>
 			            <td>9,50 €</td>
