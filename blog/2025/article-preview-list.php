8b4e79a6fd0615d7033f677fff01aa53a080c7f0
harald <harald@niemeczek.at>
2026-02-01 01:58:12 +0100

    Fixed new blog entries

--- blog/2025/article-preview-list.php
+++ blog/2025/article-preview-list.php
@@ -1,6 +1,6 @@
 <?php
 include "2025_06_20_preview.php";
-include "2025_06_20_preview.php";
+include "2025_06_17_preview.php";
 include "2025_05_01_preview.php";
 include "2025_04_20_preview.php";
 include "2025_03_30_preview.php";
