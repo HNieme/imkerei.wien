fffe5e9e5c933f2e762b420f14a601d2c56100c9
harald <harald@niemeczek.at>
2026-02-01 01:56:54 +0100

    Fixed new blog entries

--- blog/2025/article-list.php
+++ blog/2025/article-list.php
@@ -1,8 +1,10 @@
 <?php
+include "2025_06_21.php";
+include "2025_06_17.php";
+include "2025_05_01.php";
 include "2025_04_20.php";
 include "2025_03_30.php";
 include "2025_03_23.php";
 //include "2025_03_16.php";
 include "2025_03_08.php";
-include "2025_05_01.php";
 ?>
--- blog/2025/article-preview-list.php
+++ blog/2025/article-preview-list.php
@@ -1,9 +1,10 @@
 <?php
+include "2025_06_21_preview.php";
+include "2025_06_20_preview.php";
+include "2025_05_01_preview.php";
 include "2025_04_20_preview.php";
 include "2025_03_30_preview.php";
 include "2025_03_23_preview.php";
 //include "2025_03_16_preview.php";
 include "2025_03_08_preview.php";
-include "2025_05_01_preview.php";
-include "2025_06_20_preview.php";
 ?>
